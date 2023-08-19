4bf14feb3198be0791de3daffb507be8bdd81586
Wixi105 <ericsampson105@gmail.com>
2023-08-19 22:45:20 +0000

    Added humanDate method to templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -15,6 +15,18 @@ type templateData struct {
 	Snippets    []*models.Snippet
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -30,7 +42,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
